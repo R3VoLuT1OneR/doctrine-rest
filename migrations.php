c7b7aeaa8ccbd5690275dcd9271475962bb561b9
Yehor Konoval <ekonoval@gmail.com>
2024-02-26 17:44:54 +0200

    Update doctrine annotations|cache packages to comply with laravel-doctrine
    Use explicit symfony cache

--- migrations.php
+++ migrations.php
@@ -1,8 +1,6 @@
 <?php
 $loader = require __DIR__ . '/vendor/autoload.php';
 
-\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
 return [
     'migrations_namespace' => 'Pz\Doctrine\Rest\Tests\Migrations',
     'migrations_directory' => __DIR__.'/tests/migrations'
